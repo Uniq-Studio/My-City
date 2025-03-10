a0c8541027db812707a96645153d17053d8d58bc
Aaron Hamilton <aaronleon28@gmail.com>
2025-03-10 19:56:18 +0000

    Created the data source and model for the Restaurant part of the app

---
+++ app/src/main/java/org/uniqstudio/mycity/data/RestaurantDataSource.kt
@@ -0,0 +1,43 @@
+package org.uniqstudio.mycity.data
+
+import org.uniqstudio.mycity.R
+import org.uniqstudio.mycity.model.Restaurant
+
+class RestaurantDataSource {
+    fun loadRestaurants(): List<Restaurant> {
+        return listOf(
+            Restaurant(
+                imageResourceId = R.drawable.ic_launcher_foreground,
+                bannerResourceId = R.drawable.ic_launcher_foreground,
+                name = R.string.kg_name,
+                description = R.string.kg_description,
+                location = R.string.kg_location,
+                rating = R.string.rating_one
+            ),
+            Restaurant(
+                imageResourceId = R.drawable.ic_launcher_foreground,
+                bannerResourceId = R.drawable.ic_launcher_foreground,
+                name = R.string.ccr_name,
+                description = R.string.ccr_description,
+                location = R.string.ccr_location,
+                rating = R.string.rating_two
+            ),
+            Restaurant(
+                imageResourceId = R.drawable.ic_launcher_foreground,
+                bannerResourceId = R.drawable.ic_launcher_foreground,
+                name = R.string.tlb_name,
+                description = R.string.tlb_description,
+                location = R.string.tlb_location,
+                rating = R.string.rating_two
+            ),
+            Restaurant(
+                imageResourceId = R.drawable.ic_launcher_foreground,
+                bannerResourceId = R.drawable.ic_launcher_foreground,
+                name = R.string.tal_name,
+                description = R.string.tal_description,
+                location = R.string.tal_location,
+                rating = R.string.rating_two
+            ),
+        )
+    }
+}
