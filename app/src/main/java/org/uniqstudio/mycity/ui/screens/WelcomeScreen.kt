42ae91144be92f88b135c9b0870d1218978f0666
Aaron Hamilton <aaronleon28@gmail.com>
2025-03-15 23:44:31 +0000

    Implement basic navigation and welcome screen

--- app/src/main/java/org/uniqstudio/mycity/ui/screens/WelcomeScreen.kt
+++ app/src/main/java/org/uniqstudio/mycity/ui/screens/WelcomeScreen.kt
@@ -1,13 +1,9 @@
 package org.uniqstudio.mycity.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import org.uniqstudio.mycity.R
 import org.uniqstudio.mycity.ui.BlankSpaceFiller
